141294ad81cf7daa822e426fe018380defb48bfb
ivonne.lasco <ivonnelasco01@gmail.com>
2023-06-18 16:25:35 +0800

    Intent

--- WelcomePage/app/src/main/java/com/example/welcomepage/MainActivity.kt
+++ WelcomePage/app/src/main/java/com/example/welcomepage/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.welcomepage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginActbtn = findViewById<Button>(R.id.login_button)
+        val signupActbtn = findViewById<Button>(R.id.signup_button)
+
+        loginActbtn.setOnClickListener{
+            val Intent = Intent(this, Login::class.java )
+        }
+
+        signupActbtn.setOnClickListener{
+            val Intent = Intent(this, Login::class.java )
+        }
     }
-    val loginActbtn = findViewById<Button>(R.id.login_button)
-    val signupActbtn = findViewById<Button>(R.id.signup_button)
 
-    loginActbtn.setOnClickListener{
-
-    }
-
-    signupActbtn.setOnClickListener{
-
-    }
 }
