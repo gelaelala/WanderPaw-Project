aa67c36a0c0c88417a2500f8e5014f98fca23565
Gwen Susa <marygwensusa@gmail.com>
2023-06-21 21:47:54 +0800

    Removed the putExtra() function line since it doesn't serve relevance in Welcome Page

--- Source Code/app/src/main/java/com/codingstuff/loginandsignup/WelcomePage.kt
+++ Source Code/app/src/main/java/com/codingstuff/loginandsignup/WelcomePage.kt
@@ -1,14 +1,8 @@
 package com.codingstuff.loginandsignup
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewAnimationUtils
-import android.widget.Button
-import com.codingstuff.loginandsignup.databinding.ActivitySignUpBinding
+import androidx.appcompat.app.AppCompatActivity
 import com.codingstuff.loginandsignup.databinding.ActivityWelcomePageBinding
 
 class WelcomePage : AppCompatActivity() {
@@ -20,17 +14,17 @@ class WelcomePage : AppCompatActivity() {
         binding = ActivityWelcomePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         binding.loginButton.setOnClickListener {
             val intent = Intent(this, LogInActivity::class.java)
             startActivity(intent)
-            intent.putExtra("key", "login_button")
             overridePendingTransition(R.anim.slide_up, R.anim.push_out)
         }
 
         binding.signupButton.setOnClickListener{
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
-            intent.putExtra("key", "signup_button")
             overridePendingTransition(R.anim.slide_up, R.anim.push_out)
         }
     }
