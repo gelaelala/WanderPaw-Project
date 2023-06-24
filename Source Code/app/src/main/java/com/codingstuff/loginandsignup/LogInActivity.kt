47b56897e59a2cb71974d262c11f9b1f734470e3
Mary Gwen G. Susa <92632207+MaryGwenSusa@users.noreply.github.com>
2023-06-25 06:01:19 +0800

    refactored log in act code
    for better readability

--- Source Code/app/src/main/java/com/codingstuff/loginandsignup/LogInActivity.kt
+++ Source Code/app/src/main/java/com/codingstuff/loginandsignup/LogInActivity.kt
@@ -23,41 +23,82 @@ class LogInActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.button.setOnClickListener {
-            val email = binding.emailEt.text.toString()
-            val password = binding.passET.text.toString()
+        setupClickListener()
+    }
 
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
-                    } else {
-                        when (val exception = it.exception) {
-                            is FirebaseAuthException -> {
-                                val errorMessage = handleException(exception)
-                                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
-                            }
-                            is FirebaseNetworkException -> {
-                                Toast.makeText(
-                                    this,
-                                    "There is a network connectivity issue. Please check your network.",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                            is FirebaseTooManyRequestsException -> {
-                                Toast.makeText(this, "Too many requests. Try again later.", Toast.LENGTH_SHORT).show()
-                            }
-                        }
-                    }
-                }
+    // function for button clicks
+    private fun setupClickListener() {
+        binding.button.setOnClickListener {
+            handleLogin()
+        }
+
+        binding.backIcon.setOnClickListener {
+            navigateToWelcomePage()
+        }
+    }
+
+    // log in verification
+    private fun handleLogin() {
+        val email = binding.emailEt.text.toString()
+        val password = binding.passET.text.toString()
+
+        if (areFieldsNotEmpty(email, password)) {
+            signInWithEmailAndPassword(email, password)
+        } else {
+            showEmptyFieldsMessage()
+        }
+    }
+
+    // empty fields
+    private fun areFieldsNotEmpty(email: String, password: String): Boolean {
+        return email.isNotEmpty() && password.isNotEmpty()
+    }
+
+    // function for logging in an account
+    private fun signInWithEmailAndPassword(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                navigateToMainActivity()
             } else {
-                Toast.makeText(this, "Fields cannot be empty!", Toast.LENGTH_SHORT).show()
+                handleLoginFailure(task.exception)
             }
         }
-        binding.backIcon.setOnClickListener {
-            val intent = Intent(this, WelcomePage::class.java)
-            startActivity(intent)
+    }
+
+    // strips firebase exception errors
+    private fun handleLoginFailure(exception: Exception?) {
+        when (exception) {
+            is FirebaseAuthException -> {
+                val errorMessage = handleException(exception)
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+            is FirebaseNetworkException -> {
+                Toast.makeText(
+                    this,
+                    "There is a network connectivity issue. Please check your network.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            is FirebaseTooManyRequestsException -> {
+                Toast.makeText(this, "Too many requests. Try again later.", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    // response for empty fields
+    private fun showEmptyFieldsMessage() {
+        Toast.makeText(this, "Fields cannot be empty!", Toast.LENGTH_SHORT).show()
+    }
+
+    // start welcome page
+    private fun navigateToWelcomePage() {
+        val intent = Intent(this, WelcomePage::class.java)
+        startActivity(intent)
+    }
+
+    // start logged in act
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
