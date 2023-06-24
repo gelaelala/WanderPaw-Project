8745fdca84ea45a7b9d9b646056f022054e90f3a
Mary Gwen G. Susa <92632207+MaryGwenSusa@users.noreply.github.com>
2023-06-24 15:52:46 +0800

    handleException function that matches error ids to error statuses

--- Source Code/app/src/main/java/com/codingstuff/loginandsignup/AuthResultStatus.kt
+++ Source Code/app/src/main/java/com/codingstuff/loginandsignup/AuthResultStatus.kt
@@ -11,3 +11,24 @@ enum class AuthResultStatus {
     TOO_MANY_REQUESTS,
     UNDEFINED
 }
+
+
+object AuthExceptionHandler {
+    //The handleException function maps specific error codes to these statuses
+    fun handleException(e: Exception): AuthResultStatus {
+        println(e.message)
+        val status: AuthResultStatus = when (e.message) {
+            "ERROR_INVALID_EMAIL" -> AuthResultStatus.INVALID_EMAIL
+            "ERROR_WRONG_PASSWORD" -> AuthResultStatus.WRONG_PASSWORD
+            "ERROR_USER_NOT_FOUND" -> AuthResultStatus.USER_NOT_FOUND
+            "ERROR_USER_DISABLED" -> AuthResultStatus.USER_DISABLED
+            "ERROR_TOO_MANY_REQUESTS" -> AuthResultStatus.TOO_MANY_REQUESTS
+            "ERROR_OPERATION_NOT_ALLOWED" -> AuthResultStatus.OPERATION_NOT_ALLOWED
+            "ERROR_EMAIL_ALREADY_IN_USE" -> AuthResultStatus.EMAIL_ALREADY_EXISTS
+            else -> AuthResultStatus.UNDEFINED
+        }
+        return status
+    }
+
+
+}
