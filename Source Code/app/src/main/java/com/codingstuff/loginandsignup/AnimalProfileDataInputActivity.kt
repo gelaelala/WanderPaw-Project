146f9b7f067f771cf8126e1471eeef1df050de9e
Mary Gwen G. Susa <92632207+MaryGwenSusa@users.noreply.github.com>
2023-07-04 20:25:54 +0800

    start handing errors in animal profile input; handle network interruptions correctly in retrieving from database in profile page

--- Source Code/app/src/main/java/com/codingstuff/loginandsignup/AnimalProfileDataInputActivity.kt
+++ Source Code/app/src/main/java/com/codingstuff/loginandsignup/AnimalProfileDataInputActivity.kt
@@ -3,7 +3,6 @@ package com.codingstuff.loginandsignup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codingstuff.loginandsignup.databinding.ActivityAnimalProfileDataInputBinding
-import com.codingstuff.loginandsignup.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -23,53 +22,45 @@ class AnimalProfileDataInputActivity : AppCompatActivity() {
         databaseRef = FirebaseDatabase.getInstance().reference
         firebaseAuth = FirebaseAuth.getInstance()
 
-
-
-
-
-
-
-
-
-
-// Write the pet card data to the database under the user's petCards node
         binding.nextButton.setOnClickListener {
-            val currentUser = firebaseAuth.currentUser
-            val userId = currentUser?.uid
-            val petCardID = createAnimalProfileId()
-            val name = binding.nameInputField.text.toString()
-            val age = binding.ageInputField1.text.toString()
-            val gender = binding.genderInputField.selectedItem
-            val location = binding.locationInputField.text.toString()
-            val bio = binding.bioInputField.text.toString()
-            val aboutMe = binding.aboutMeInputField.text.toString()
+            try {
+                val currentUser = firebaseAuth.currentUser
+                val userId = currentUser?.uid
+                val petCardID = createAnimalProfileId()
+                val name = binding.nameInputField.text.toString()
+                val age = binding.ageInputField1.text.toString()
+                val gender = binding.genderInputField.selectedItem
+                val location = binding.locationInputField.text.toString()
+                val bio = binding.bioInputField.text.toString()
+                val aboutMe = binding.aboutMeInputField.text.toString()
 
-            // Create a HashMap to store user data
-            val petCardData = HashMap<String, Any>()
-            petCardData["Name"] = name
-            petCardData["Age"] = age
-            petCardData["Gender"] = gender
-            petCardData["Location"] = location
-            petCardData["Bio"] = bio
-            petCardData["About Me"] = aboutMe
+                // Create a HashMap to store user data
+                val petCardData = HashMap<String, Any>()
+                petCardData["Name"] = name
+                petCardData["Age"] = age
+                petCardData["Gender"] = gender
+                petCardData["Location"] = location
+                petCardData["Bio"] = bio
+                petCardData["About Me"] = aboutMe
 
-            if (userId != null) {
-                databaseRef.child("Users").child(userId).child("Animal Profiles Created").child(petCardID)
-                    .updateChildren(petCardData)
+                if (userId != null) {
+                    databaseRef.child("Users").child(userId).child("Animal Profiles Created").child(petCardID)
+                        .updateChildren(petCardData)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                // Pet card data successfully written to the database
+                                // Perform any additional actions or show success message
+                            } else {
+                                // Error occurred while writing pet card data to the database
+                                // Handle the error appropriately (e.g., show error message)
+                            }
+                        }
+                }
+            } catch (e: Exception) {
+                // Handle any exceptions that occur during data saving
+                e.printStackTrace()
             }
         }
-//            .addOnCompleteListener { task ->
-//                if (task.isSuccessful) {
-//                    // Pet card data successfully written to the database
-//                    // Perform any additional actions or show success message
-//                } else {
-//                    // Error occurred while writing pet card data to the database
-//                    // Handle the error appropriately (e.g., show error message)
-//                }
-//            }
-
-
-
     }
 }
 
--- Source Code/app/src/main/java/com/codingstuff/loginandsignup/ProfilePage.kt
+++ Source Code/app/src/main/java/com/codingstuff/loginandsignup/ProfilePage.kt
@@ -1,8 +1,13 @@
 package com.codingstuff.loginandsignup
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.codingstuff.loginandsignup.databinding.ActivityProfilePageBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -18,7 +23,7 @@ class ProfilePage : AppCompatActivity() {
     private lateinit var databaseRef: DatabaseReference
     private val authToastLess = AuthToastLess(this)
 
-
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfilePageBinding.inflate(layoutInflater)
@@ -30,15 +35,20 @@ class ProfilePage : AppCompatActivity() {
         val currentUser = firebaseAuth.currentUser
         val userId = currentUser?.uid
 
-        userId?.let { retrieveUserData(it)
+        if (isNetworkConnected()) {
+            userId?.let { retrieveUserData(it) }
+        } else {
+            // Handle no internet connection case
+            // Display an appropriate message or take necessary actions
+            Toast.makeText(this,"There is a network connectivity issue. Please check your network.", Toast.LENGTH_LONG).show()
+        }
 
-        binding.addPetProfileButton.setOnClickListener{
+        binding.addPetProfileButton.setOnClickListener {
             val intent = Intent(this, AnimalProfileDataInputActivity::class.java)
             startActivity(intent)
-        }}
+        }
     }
 
-    // change the listener if there is time for settings
     private fun retrieveUserData(userId: String) {
         databaseRef.child("Users").child(userId)
             .addListenerForSingleValueEvent(object : ValueEventListener {
@@ -65,6 +75,16 @@ class ProfilePage : AppCompatActivity() {
                 }
             })
     }
+// checks for network connectivity before retrieving form the database
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun isNetworkConnected(): Boolean {
+        val connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork
+        val capabilities = connectivityManager.getNetworkCapabilities(network)
+        return capabilities != null &&
+                (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+    }
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
@@ -72,5 +92,7 @@ class ProfilePage : AppCompatActivity() {
         finishAffinity()
     }
 }
+
+
 
 
