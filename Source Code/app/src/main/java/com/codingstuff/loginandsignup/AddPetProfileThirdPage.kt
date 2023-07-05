392e4599c36f5a12de70b6199f6294393508b047
Angela Corpuz <angelaescariocorpuz@gmail.com>
2023-07-05 22:57:32 +0800

    Created three activities for Add Pet Information page and started adding input fields on first page

---
+++ Source Code/app/src/main/java/com/codingstuff/loginandsignup/AddPetProfileThirdPage.kt
@@ -0,0 +1,11 @@
+package com.codingstuff.loginandsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPetProfileThirdPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pet_profile_third_page)
+    }
+}
