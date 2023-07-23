7550285a3d6460ac765878c12394900992986c47
ivonne.lasco <ivonnelasco01@gmail.com>
2023-07-23 09:31:03 +0800

    latest working camera permission

--- Source Code/app/src/main/java/com/codingstuff/loginandsignup/AddPetInformation.kt
+++ Source Code/app/src/main/java/com/codingstuff/loginandsignup/AddPetInformation.kt
@@ -2,9 +2,11 @@ package com.codingstuff.loginandsignup
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.Uri
@@ -22,6 +24,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import com.codingstuff.loginandsignup.AuthExceptionHandler.Companion.handleException
@@ -50,6 +54,8 @@ class AddPetInformation : AppCompatActivity() {
     private val otherNeedsTextList = mutableListOf<TextInputEditText>()
     private val requirementsTextList = mutableListOf<TextInputEditText>()
     private val contactInfoTextList = mutableListOf<TextInputEditText>()
+
+    val CAMERA_RQ = 102
 
     private val authToastLess = AuthToastLess(this)
     private lateinit var imageUri: Uri
@@ -147,10 +153,6 @@ class AddPetInformation : AppCompatActivity() {
 
             override fun onNothingSelected(parent: AdapterView<*>) {
             }
-        }
-
-        binding.imageButton.setOnClickListener {
-            openFileChooser()
         }
 
         // Write the pet card data to the database under the user's petCards node
@@ -255,6 +257,58 @@ class AddPetInformation : AppCompatActivity() {
         binding.addMedCon.setOnClickListener{
             addNewMedConInputField()
         }
+
+        textViewtaps()
+    }
+
+    private fun textViewtaps(){
+        val image = findViewById<CardView>(R.id.imageButton)
+        image.setOnClickListener{
+            checkForPermission(android.Manifest.permission.CAMERA,"camera", CAMERA_RQ)
+        }
+    }
+
+    private fun checkForPermission(permission: String, name: String, requestCode: Int){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            when{
+                ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED -> {
+                    Toast.makeText(applicationContext, "Upload Image", Toast.LENGTH_SHORT).show()
+                    openFileChooser()
+                }
+                shouldShowRequestPermissionRationale(permission) -> showDialog(permission, name, requestCode)
+
+                else -> ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        fun innerCheck(name: String){
+            if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED ){
+                Toast.makeText(applicationContext, "$name permission refused", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext, "$name permission granted", Toast.LENGTH_SHORT).show()
+                openFileChooser()
+            }
+        }
+        when (requestCode){
+            CAMERA_RQ -> innerCheck("Camera")
+        }
+    }
+
+    private fun showDialog(permission: String, name: String, requestCode: Int){
+        val builder = AlertDialog.Builder(this)
+
+        builder.apply {
+            setMessage("Permission to access your $name is required to use this app")
+            setTitle("Permission required")
+            setPositiveButton("OK") { dialog, which ->
+                ActivityCompat.requestPermissions(this@AddPetInformation, arrayOf(permission), requestCode)
+            }
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 
     private fun formatStringWithCapital(input: String): String {
