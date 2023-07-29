309ffba5cc479cae3eb3b9a2602d77b0930bf8a9
ivonne.lasco <ivonnelasco01@gmail.com>
2023-07-29 10:59:19 +0800

    google authentication with realtime

--- Source Code/app/src/main/java/com/codingstuff/loginandsignup/LogInActivity.kt
+++ Source Code/app/src/main/java/com/codingstuff/loginandsignup/LogInActivity.kt
@@ -2,16 +2,28 @@ package com.codingstuff.loginandsignup
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.codingstuff.loginandsignup.AuthExceptionHandler.Companion.handleException
 import com.codingstuff.loginandsignup.databinding.ActivityLogInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class LogInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogInBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private val authToastLess = AuthToastLess(this)
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var databaseRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +31,18 @@ class LogInActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        databaseRef = FirebaseDatabase.getInstance().reference
 
         setupClickListener()
-    }
 
+        // Configure Google Sign-In options
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+    }
     // function for button clicks
     private fun setupClickListener() {
         binding.button.setOnClickListener {
@@ -32,6 +52,69 @@ class LogInActivity : AppCompatActivity() {
         binding.backIcon.setOnClickListener {
             navigateToWelcomePage()
         }
+
+        binding.googleBtn.setOnClickListener{
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                val displayName = getDisplayNameFromAccount(account)
+                firebaseAuthWithGoogle(account.idToken!!, displayName)
+            }catch (e: ApiException){
+                Toast.makeText(this, "Google Sign In failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getDisplayNameFromAccount(account: GoogleSignInAccount): String {
+        val givenName = account.givenName ?: ""
+        val familyName = account.familyName ?: ""
+        return "$givenName $familyName"
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String, displayName: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful){
+                    val user = firebaseAuth.currentUser
+                    if(user!=null){
+                        val databaseRef = FirebaseDatabase.getInstance().reference
+                        val userRef = user?.let { databaseRef.child("Users").child(it.uid) }
+                        if (userRef != null) {
+                            userRef.child("Display Name").setValue(displayName)
+                                .addOnSuccessListener {
+                                    // Name stored in the database successfully
+                                    navigateToHomePage()
+                                }
+                                .addOnFailureListener { exception ->
+                                    // Error occurred while storing the name
+                                    Toast.makeText(this, "Error storing name in the database", Toast.LENGTH_SHORT).show()
+                                }
+                        }else{
+                            handleLoginFailure(task.exception)
+                        }
+                    }
+                }else{
+                    handleLoginFailure(task.exception)
+                }
+            }
+    }
+
+    companion object{
+        const val RC_SIGN_IN = 1001
     }
 
     // log in verification
@@ -81,6 +164,13 @@ class LogInActivity : AppCompatActivity() {
         val intent = Intent(this, WelcomePage::class.java)
         startActivity(intent)
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+        finish()
+    }
+
+    private fun navigateToLoginPage() {
+        val intent = Intent(this, LogInActivity::class.java)
+        overridePendingTransition(R.anim.stay, R.anim.stay)
+        startActivity(intent)
         finish()
     }
 
